b6b7a43ff25412e5e14277c663f9517c29755197
Idriss <hadraskat@gmail.com>
2022-05-03 12:35:20 +0200

    v2 finall

--- app.js
+++ app.js
@@ -1,57 +1,78 @@
 'use strict';
 
 // Api https://gist.github.com/bertez/8e62741154903c35edb3bfb825a7f052
+const timeLineList = document.querySelector('#timelineList');
+const searchBar = document.querySelector('#searchBar');
+let data = [];
 
-async function getEvents() {
+// Search
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value;
+  const filterTimeline = data.filter((item) => {
+    return (
+      item.date.toString().toLowerCase().includes(searchString) ||
+      item.title.toLowerCase().includes(searchString)
+    );
+  });
+  displayTimeline(filterTimeline);
+});
+
+// Fetch data
+const loadTimeline = async () => {
   try {
     const response = await fetch(
       'https://gist.githubusercontent.com/bertez/8e62741154903c35edb3bfb825a7f052/raw/b5cd5137fd168116cc71740f1fbb75819d0fa82e/zelda-timeline.json'
     );
-    const data = await response.json();
-    // ordenarlos en el tiempo
+    data = await response.json();
     data.sort((a, b) => {
       return a.date - b.date;
     });
-    // type it to the DOM
-    data.forEach((event) => {
-      const eventElement = document.createElement('li');
-      eventElement.innerHTML = `<div>
-      <time>${event.date}</time>
-        <h1>${event.title}</h1>
-        <img src="${event.image}" alt="image">
-      <p>${event.text}</p>
-      </p>
-  </div>`;
-      return document.querySelector('ul').appendChild(eventElement);
-    });
-    const items = document.querySelectorAll('li');
-    function isItemInView(item) {
-      const rect = item.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-          (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-    function callbackFunc() {
-      for (let i = 0; i < items.length; i++) {
-        if (isItemInView(items[i])) {
-          items[i].classList.add('show');
-        }
-      }
-    }
-    window.addEventListener('load', callbackFunc);
-    window.addEventListener('resize', callbackFunc);
-    window.addEventListener('scroll', callbackFunc);
+    displayTimeline(data);
+    console.log(data);
   } catch (error) {
     console.log(error);
   }
-}
-getEvents();
+};
 
-console.log(data);
-
-// listen for events
+// Display data
+const displayTimeline = (timeLine) => {
+  const htmlString = timeLine
+    .map((item) => {
+      return `
+    <li class="timeline-item">
+    <div>
+      <time>${item.date}</time>
+        <h2>${item.title}</h2>
+        <img src="${item.image}" alt="image">
+      <p>${item.text}</p>
+      </p>
+  </div>
+  </li>`;
+    })
+    .join('');
+  timeLineList.innerHTML = htmlString;
+  // effect animation
+  const items = document.querySelectorAll('li');
+  function isItemInView(item) {
+    const rect = item.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  function callbackFunc() {
+    for (let i = 0; i < items.length; i++) {
+      if (isItemInView(items[i])) {
+        console.log(items[i]);
+        items[i].classList.add('show');
+      }
+    }
+  }
+  window.addEventListener('load', callbackFunc);
+  window.addEventListener('resize', callbackFunc);
+  window.addEventListener('scroll', callbackFunc);
+};
+loadTimeline();
